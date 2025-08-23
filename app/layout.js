3513f3123903ed533551caf3eecef404bf6e1c8e
Akash Prodhan <akashprodhan78@gmail.com>
2025-08-23 16:19:20 +0530

    crate a custon 404 page

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 export const metadata = {
   title: {
     template: "%s | Technicle Agency",
-    // default : "Technicle Agency"
+    default : "Technicle Agency"
   }
 }
 
